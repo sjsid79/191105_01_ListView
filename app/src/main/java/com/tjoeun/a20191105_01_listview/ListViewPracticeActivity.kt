ff688d89504379718653c28f4bad9031d7a8b265
sjsid79 <wjdtlr0723>
2019-11-05 21:55:27 +0900

    액티비티에서 어댑터 객체화, 리스트뷰와 연결

--- app/src/main/java/com/tjoeun/a20191105_01_listview/ListViewPracticeActivity.kt
+++ app/src/main/java/com/tjoeun/a20191105_01_listview/ListViewPracticeActivity.kt
@@ -2,17 +2,24 @@ package com.tjoeun.a20191105_01_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191105_01_listview.adapters.PostAdapter
 import com.tjoeun.a20191105_01_listview.datas.PostData
+import kotlinx.android.synthetic.main.activity_list_view_practice.*
 
 class ListViewPracticeActivity : AppCompatActivity() {
 
     var postList = ArrayList<PostData>()
+    var postAdapter:PostAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view_practice)
 
         addPosts()
+
+        postAdapter = PostAdapter(this, R.layout.post_list_item, postList)
+        postListView.adapter = postAdapter
+
     }
 
     fun addPosts(){
