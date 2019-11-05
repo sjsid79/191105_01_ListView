6e94c4f768babc85d228906031477c741cbcf4a2
sjsid79 <wjdtlr0723>
2019-11-05 20:31:31 +0900

    방 데이터 클래스 추가   - 메인 액티비티 : 데이터 추가를 별개 메소드로 작성   - sum메소드로 코틀리만의 특이한 문법 확인

--- app/src/main/java/com/tjoeun/a20191105_01_listview/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191105_01_listview/MainActivity.kt
@@ -2,11 +2,50 @@ package com.tjoeun.a20191105_01_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191105_01_listview.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    var roomList = ArrayList<RoomData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addRooms()
     }
+    // Ex. 두 숫자의 함계를 리턴해주는 함수 생성
+    fun sumFun(num1:Int, num2:Int):Int {
+        val result = num1 + num2
+        return  result
+    }
+    //위와 동일하게 실행된다.
+//    fun sumFun(num1:Int, num2:Int) = num1 + num2
+
+
+    //자바에선 레코드, 코틀린은 함수
+    fun addRooms(){
+        var room1 = RoomData()
+        room1.price = 8000
+        room1.address = "마포구 서교동"
+        room1.floor = 1
+        roomList.add(room1)
+
+        //Alt를 누른상태에서 드래그하면 한번에 수정가능.
+        var room2 = RoomData()
+        room2.price = 28500
+        room2.address = "마포구 서교동"
+        room2.floor = 3
+        roomList.add(room2)
+
+        var room3 = RoomData()
+        room3.price = 12000
+        room3.address = "마포구 성산동"
+        room3.floor = 5
+        roomList.add(room3)
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/tjoeun/a20191105_01_listview/datas/RoomData.kt
@@ -0,0 +1,9 @@
+package com.tjoeun.a20191105_01_listview.datas
+
+class RoomData {
+
+    var price:Int = 0
+    var address:String = ""
+    var floor:Int = 1   // 음수 : 지하. -1층 : 지하 1층. 0층 : 반지하
+
+}
