1cb22e87bb8d61760fdd9c70318a670ec0c6c987
sjsid79 <wjdtlr0723>
2019-11-05 21:39:43 +0900

    새 액티비티(ListViewPracticeActivity) 생성. 리스트뷰 생성

---
+++ app/src/main/java/com/tjoeun/a20191105_01_listview/ListViewPracticeActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191105_01_listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListViewPracticeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view_practice)
+    }
+}
