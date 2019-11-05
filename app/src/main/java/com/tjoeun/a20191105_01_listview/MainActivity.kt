70fb18bcdb42cf358c5a34d1d6e317d127c3ddd4
sjsid79 <wjdtlr0723>
2019-11-05 19:54:08 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191105_01_listview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191105_01_listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
